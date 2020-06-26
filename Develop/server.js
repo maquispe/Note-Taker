ab38f51b5b6cd122c24fcb4746d91ffbf0802103
maquispe <62685159+maquispe@users.noreply.github.com>
2020-06-26 00:49:42 -0700

    Adds routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("/api/notes", (req, res) => {
-  res.json(path.join(__dirname, "/db/db.json"));
-});
+app.use(express.static("public"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT,() => {
   console.log("App listening on PORT: " + PORT);
